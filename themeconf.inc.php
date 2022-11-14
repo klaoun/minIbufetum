3b64eafb8b9aa712e255e31c3ad66657d5fae85a
klaoun <36087587+klaoun@users.noreply.github.com>
2022-11-14 01:07:39 +0100

    Create themeconf.inc.php

---
+++ themeconf.inc.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Theme Name: Ibufetum
+Version: auto
+Description: 2ème thème personnalisé de piwigo
+Theme URI:
+Author: klaoun
+Author URI:
+Has Settings: true
+*/
+$themeconf = array(
+  'name' => 'minIbufetum',
+  'parent' => 'default',
+  'icon_dir' => 'themes/minIbufetum/img',
+  'img_dir'      => 'themes/minIbufetum/img',
+  'mime_icon_dir' => 'themes/minIbufetum/img/',
+  'activable' => 'true',
+  'load_parent_local_head' => 'false',
+  'load_parent_css' => 'false'
+);
+load_language('theme.lang', PHPWG_THEMES_PATH.'minIbufetum/');
+?>
