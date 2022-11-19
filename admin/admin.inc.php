2424d589cd912116e3ece6678eec496ba3eca6e5
klaoun <36087587+klaoun@users.noreply.github.com>
2022-11-19 02:18:20 +0100

    Update admin.inc.php

--- admin/admin.inc.php
+++ admin/admin.inc.php
@@ -1,3 +1,8 @@
 <?php
+$template->set_filenames(array('theme_admin_content' => dirname(__FILE__) . '/admin.tpl'));
+$template->assign_var_from_handle('ADMIN_CONTENT', 'theme_admin_content');
+
+define('THEME_ID', basename(dirname(dirname(__FILE__))));
+define('ADMIN_PATH',   get_root_url() . 'admin.php?page=theme&theme=' . THEME_ID);
 
 ?>
